4f0c1eb2b792f4286d4e68f865f6288c17a410ed
KanahS <kanah.smith@mail.utoronto.ca>
2022-12-06 15:07:45 -0500

    updates up to early December

--- class_Q4.py
+++ class_Q4.py
@@ -12,8 +12,8 @@ e_b = np.linspace(0, 0.8, tup_num)
 a_p = np.linspace(1, 5, tup_num)
 Np = 15
 
-Qex = [np.inf]
-tidal_lag = [1,2,3,4]
+Qex = []
+tidal_lag = [4]
 for x in tidal_lag:
     Q = 10**x
     Qex.append(Q)
@@ -68,10 +68,10 @@ def survival(initial):
     else:
         tau = ps[0].params["tctl_tau"] = 3/(2*Q*k2*nb)
     
-    #directory_orbit = "/mnt/raid-cita/ksmith/COPE_OR/" # COPE
-    #rebx.save(directory_orbit+f"xarchive_Q{Q}_original_radius.bin") # COPE  # TURN BACK ON IF RUNNING ANOTHER SIM FOR DIFFERENT Q VALUES OR OTHER CHANGES
-    #filename_orbit = r"eb{:.3f}_ap{:.3f}_Np{:.1f}_tup{:.1f}_Q{:.1f}_tau{:.4f}_original_radius.bin".format(eb,ap,Np,tup_num,Q,tau) # COPE
-    #sim.automateSimulationArchive(directory_orbit+filename_orbit, interval=1e3, deletefile=True) # COPE
+    directory_orbit = "/mnt/raid-cita/ksmith/cope/" # COPE
+    rebx.save(directory_orbit+f"xarchive_single_Qs{Q}.bin") # COPE  # TURN BACK ON IF RUNNING ANOTHER SIM FOR DIFFERENT Q VALUES OR OTHER CHANGES
+    filename_orbit = r"eb{:.3f}_ap{:.3f}_Np{:.1f}_tup{:.1f}_Q{:.1f}_tau{:.4f}.bin".format(eb,ap,Np,tup_num,Q,tau) # COPE
+    sim.automateSimulationArchive(directory_orbit+filename_orbit, interval=1e3, deletefile=True) # COPE
 
     
     #integrate
@@ -103,32 +103,26 @@ def survival(initial):
             d1 = np.sqrt((p.x - p1.x)**2 + (p.y - p1.y)**2)
 
             if d > 20 or d0 < 0.25 or d1 < 0.25 or o.e > 1:
-           #if (p.x**2 + p.y**2) > (100)**2 or p.e > 1: # EDITING ORBITAL PARAMS HERE
                 surv[num-2] = time
-               # print(f'removing planet {num}')
                 sim.remove(num)
-
             if sim.N==2:
                 break
     surv[(surv==0)] = time
     
-    #print(f'simulation finished, {len(sim.particles)-2} planets remaining')
-   
     # Saving raw survival times
-    
-    #directory_surv = "/mnt/raid-cita/ksmith/CSTE_OR/" #CSTE
-    #file_surv = "raw_surv_time_eb{:.3f}_ap{:.3f}_Q{:.1f}_original_radius.npy".format(eb,ap,Q) #CSTE
-    #np.savetxt(directory_surv+file_surv, surv) #CSTE
+    directory_surv = "/mnt/raid-cita/ksmith/cste/" #CSTE
+    file_surv = "raw_surv_time_Q{:.1f}_eb{:.3f}_ap{:.3f}.npy".format(Q,eb,ap) #CSTE
+    np.savetxt(directory_surv+file_surv, surv) #CSTE
     return np.mean(surv)
    
 pool = rb.InterruptiblePool(processes=32)
 mapping = pool.map(func= survival, iterable= tup_list)
 
-directory_surv = "/mnt/raid-cita/ksmith/CSTE_OR/"# CSTE
-npy_surv = f"map_tup{tup_num}plan{Np}_Qs{Qex[:]}_original_radius.npy" # CSTE
+directory_surv = "/mnt/raid-cita/ksmith/cste/"# CSTE
+npy_surv = f"map_tup{tup_num}plan{Np}_Q{Qex[:]}.npy" # CSTE
 np.savetxt(directory_surv+npy_surv, mapping) # CSTE
 
 directory_test = '/mnt/raid-cita/ksmith/'# DONE
 completed = 'The simulation finished!'# DONE
-name = 'CSTE_map_DONE' # DONE
+name = 'map_Q4_DONE' # DONE
 np.save(directory_test+name, completed) # DONE
