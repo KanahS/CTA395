d33a48dc90b2fcaeb32f332f1e90fe661f197c3a
KanahS <kanah.smith@mail.utoronto.ca>
2023-03-09 16:04:17 -0500

    Added new directories and ,odified class_Q1 to grab the high timestep data

--- class_Q1.py
+++ class_Q1.py
@@ -8,8 +8,8 @@ import rebound as rb
 import reboundx as rx
 
 tup_num = 50   
-e_b = np.linspace(0, 0.8, tup_num)
-a_p = np.linspace(1, 5, tup_num)
+e_b = [0.473]  #np.linspace(0, 0.8, tup_num)
+a_p = [5]  #np.linspace(1, 5, tup_num)
 Np = 15
 
 Qex = []
@@ -70,16 +70,16 @@ def survival(initial):
         tau = ps[0].params["tctl_tau"] = 3/(2*Q*k2*nb)
     
     # SIMULATION ARCHIVE ###filename_orbit = r"eb{:.3f}_ap{:.3f}_Np{:.1f}_tup{:.1f}_Q{:.1f}_tau{:.4f}.npy".format(eb,ap,Np,tup_num,Q,tau) # COPE
-
-    #directory_orbit = "/mnt/raid-cita/ksmith/cope/" # COPE
-    #rebx.save(directory_orbit+f"xarchive_single_Qs{Q}.bin")# rebx archive # COPE  # TURN BACK ON IF RUNNING ANOTHER SIM FOR DIFFERENT Q VALUES OR OTHER CHANGES
-    #filename_orbit = r"sim_archive_Q{:.1f}_eb{:.3f}_ap{:.3f}.bin".format(Q,eb,ap)# reb archive # cope 
-    #sim.automateSimulationArchive(directory_orbit+filename_orbit, interval=1e-2, deletefile=True) # COPE # for the old stuff do interval = 1e-3
+    #directory_orbit = "/mnt/raid-cita/ksmith/cope/" # cope
+    directory_orbit = "/mnt/raid-cita/ksmith/cope_high-timestep/" # cope_high-timestep
+    rebx.save(directory_orbit+f"xarchive_single_Qs{Q}.bin")# rebx archive # COPE  # TURN BACK ON IF RUNNING ANOTHER SIM FOR DIFFERENT Q VALUES OR OTHER CHANGES
+    filename_orbit = r"sim_archive_Q{:.1f}_eb{:.3f}_ap{:.3f}.bin".format(Q,eb,ap)# reb archive # cope 
+    sim.automateSimulationArchive(directory_orbit+filename_orbit, interval=1e-2, deletefile=True) # COPE # for the old stuff do interval = 1e-3, for the new stuff do interval = 1e-2
 
     
     #integrate
     N_times = int(10000) 
-    N_orbit = (1e4)*2*np.pi  #(1e4)*2*np.pi # for the new stuff, do (1e3)*2*np.pi
+    N_orbit = (1e3)*2*np.pi  #(1e4)*2*np.pi for the regular stuff # for the new stuff, do (1e3)*2*np.pi
     times = np.linspace(0,N_orbit,N_times)
 
     #array for survival times
@@ -119,13 +119,14 @@ def survival(initial):
     surv[(surv==0)] = time
    
     # SAVING RAW SURVIVAL TIMES
-    directory_surv = "/mnt/raid-cita/ksmith/cste/" #cste
+    #directory_surv = "/mnt/raid-cita/ksmith/cste/" #cste
+    directory_surv = "/mnt/raid-cita/ksmith/cste_high-timestep/" # cste_high-timestep
     file_surv = r"raw_surv_time_Q{:.1f}_eb{:.3f}_ap{:.3f}.npy".format(Q,eb,ap) #cste
     np.savetxt(directory_surv+file_surv, surv) #cste
 
     # SAVING BINARY ECCENTRICITIES W/ ebs, should only end up w/ 50 files as ap changes and eb remains the same, so the overwrite each other, don't bother trying to selectively have only 1 file for 1 eb
-    #file_ebs = "binary_evolution_initial_ebs{:.3f}.npy".format(eb) # COPE
-    #np.savetxt(directory_orbit+file_ebs,ebs) # COPE
+    file_ebs = r"binary_evolution-initial_ebs{:.3f}_Q{Q}.npy".format(eb,Q) # cope
+    np.savetxt("/mnt/raid-cita/ksmith/cope/"+file_ebs,ebs) # cope
     
     return np.mean(surv)
    
