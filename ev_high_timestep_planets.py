9e78abfd588db67d0b04c66a8e8033eab28652c8
KanahS <kanah.smith@mail.utoronto.ca>
2023-03-30 22:07:13 -0400

    March 30th, 2023

---
+++ ev_high_timestep_planets.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import rebound as rb
+import reboundx as rx
+import matplotlib.pyplot as plt
+
+
+e_val = 0.702 # INITIAL PLANETARY SEMI-MAJOR AXIS UNDER EVALUATION
+a_val = 2.878 # INITIAL BINARY ECCENTRICITY UNDER EVALUATION
+
+high_xarch = "/mnt/raid-cita/ksmith/cope_high-timestep/xarchive_single_Qs10.bin"
+
+def planet_evolution(archive, extras):
+
+    sim = None
+    sim = rx.SimulationArchive(archive, rebxfilename=extras) #rb.SimulationArchive(archive)
+
+    planet_semi = []
+    planet_ecc = []
+    sim_time = []
+
+    for planet in range(2,17):
+        a = []
+        e = []
+        t = []
+        #for snap in range(len(sim)):
+        for snap in range(len(2)):    
+            try:
+                base = sim[snap][0]
+                time = base.t
+
+                plan_a = base.particles[planet].a
+                plan_e = base.particles[planet].e
+
+                a.append(plan_a)
+                e.append(plan_e)
+                t.append(time)
+                #print("Particle Found")
+
+            except: #ParticleNotFound: # move onto the next iteration/planet
+                continue  # DO THE LIST INDEX ERROR
+                #print(f"In snap {snap+1} Planet {planet-2} Not Found")
+
+        planet_semi.append(a)
+        planet_ecc.append(e)
+        sim_time.append(t)
+    
+    np.save(high_dir_cope+f"sim_length_is_{len(sim)}", len(sim))
+    np.save(high_dir_cope+"sim_time.npy", sim_time)
+    np.save(high_dir_cope+"planet_semi.npy", planet_semi)
+    np.save(high_dir_cope+"planet_ecc.npy", planet_ecc)
+
+    #return sim_time, planet_semi, planet_ecc 
+
+
+high_sim_cste = f"/mnt/raid-cita/ksmith/cste_high-timestep/raw_surv_time_Q10.0_eb{e_val}_ap{a_val}.npy"
+
+# planetary stuff
+#plan = planet_evolution(high_sim_cste, high_xarch)
+# np.save(high_sim_planets+"high_sim_planets.npy", plan)
+planet_evolution(high_sim_cste, high_xarch)
+
--- planet_hist.py
+++ planet_hist.py
@@ -23,11 +23,8 @@ hist_sim_arch_directory = "/mnt/raid-cita/ksmith/cope/"
 hist_xarch = "/mnt/raid-cita/ksmith/cope/xarchive_single_Qs10.bin"
 
 def planet_hist(sim_arch_directory, xarch):
-    last_snapshot = 63
-    first_snapshot = 0
-
-    ep_final = []
-    ap_fmi = [] # final minus initial 
+    ep_final_below = []
+    ap_fmi_below = [] # final minus initial 
 
     for num in range(tup_num): # tup_num = 50
         eb_i = e_hist[num]
@@ -35,27 +32,28 @@ def planet_hist(sim_arch_directory, xarch):
         # mu = 0.5
         a_c = 1.6 + 5.1*eb_i + (- 2.22*(eb_i**2)) + 4.12*0.5 + (- 4.27*eb_i*0.5) + (- 5.09*(0.5**2)) + 4.61*(eb_i**2)*(0.5**2)
         
-        archive = None
-        archive = hist_sim_arch_directory + f"sim_archive_Q10.0_eb{e_string[num]}_ap{a_string[num]}.npy"
+        #archive = None
+        #archive = sim_arch_directory + f"sim_archive_Q10.0_eb{e_string[num]}_ap{a_string[num]}.npy"
         if ap_i <= a_c:
+            archive = sim_arch_directory + f"sim_archive_Q10.0_eb{e_string[num]}_ap{a_string[num]}.npy"
             sim = None
             sim = rx.SimulationArchive(archive, rebxfilename=xarch)
         
             for planet in range(2,17):
-                if len(sim) == last_snapshot: # last snap that there should be for all full length simulations 
+                if len(sim) == 63: # last snap that there should be for all full length simulations 
                     try:
                         a_initial = sim[0][0].particles[planet].a
                         a_final = sim[62][0].particles[planet].a
                         a_final_minus_initial = a_final - a_initial
-                        ap_fmi.append(a_final_minus_initial)
+                        ap_fmi_below.append(a_final_minus_initial)
                 
                         e_final = sim[62][0].particles[planet].e
-                        ep_final.append(e_final)
+                        ep_final_below.append(e_final)
                     
                     except:
                         continue
 
-    np.save("/mnt/raid-cita/ksmith/planet_hist_ep_final.npy", ep_final)
-    np.save("/mnt/raid-cita/ksmith/planet_hist_ap_fmi.npy", ap_fmi)
+    np.save("/mnt/raid-cita/ksmith/planet_hist_ep_final_below.npy", ep_final_below)
+    np.save("/mnt/raid-cita/ksmith/planet_hist_ap_fmi_below.npy", ap_fmi_below)
 
 planet_hist(hist_sim_arch_directory, hist_xarch)
