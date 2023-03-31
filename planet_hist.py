761821e0e18d5fa2779d87fb724ec50245d3c768
KanahS <kanah.smith@mail.utoronto.ca>
2023-03-31 02:21:09 -0400

    March 31st, 2023

--- planet_hist.py
+++ planet_hist.py
@@ -26,6 +26,9 @@ def planet_hist(sim_arch_directory, xarch):
     ep_final_below = []
     ap_fmi_below = [] # final minus initial 
 
+    ep_final_above = []
+    ap_fmi_above = []
+
     for num in range(tup_num): # tup_num = 50
         eb_i = e_hist[num]
         ap_i = a_hist[num]
@@ -52,6 +55,27 @@ def planet_hist(sim_arch_directory, xarch):
                     
                     except:
                         continue
+        if ap_i > a_c:
+            archive = hist_sim_arch_directory + f"sim_archive_Q10.0_eb{e_string[num]}_ap{a_string[num]}.npy"
+        sim = None
+        sim = rx.SimulationArchive(archive, rebxfilename=hist_xarch) #rb.SimulationArchive(archive)
+
+        for planet in range(2,17):
+            if len(sim) == 63: # last snap that there should be for all full length simulations
+                try:
+                    a_initial = sim[0][0].particles[planet].a
+                    a_final = sim[62][0].particles[planet].a
+                    a_final_minus_initial = a_final - a_initial
+                    ap_fmi_above.append(a_final_minus_initial)
+
+                    e_final = sim[62][0].particles[planet].e
+                    ep_final_above.append(e_final)
+
+                except: 
+                    continue 
+              
+    np.save("/mnt/raid-cita/ksmith/planet_hist_ep_final_above.npy", ep_final_above)
+    np.save("/mnt/raid-cita/ksmith/planet_hist_ap_fmi_above.npy", ap_fmi_above)
 
     np.save("/mnt/raid-cita/ksmith/planet_hist_ep_final_below.npy", ep_final_below)
     np.save("/mnt/raid-cita/ksmith/planet_hist_ap_fmi_below.npy", ap_fmi_below)
